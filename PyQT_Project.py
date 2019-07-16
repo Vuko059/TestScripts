afdb00b52b50ed915cff0643800ea02684433261
CHOCHÓŁ <MATEUSZ.CHOCHOL@ing.com>
2019-07-16 15:05:34 +0200

    Well working..

--- PyQT_Project.py
+++ PyQT_Project.py
@@ -2,9 +2,39 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-#from run import *
-
+from Rooms import room
 #///////libraries///////libraries///////libraries///////libraries////////
+
+nick = " YOU "
+
+class Main(QWidget):
+    window = None
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.Start()
+
+    def Start(self):
+        Btngo = QPushButton("&Start", self)
+        Btngo.clicked.connect(self.tt)
+        self.resize(400, 300)
+        self.move(300, 300)
+        self.setWindowTitle('Start')
+        self.show()
+
+
+
+    def tt(self):
+        global window
+        window = Program()
+        window.show()
+        self.close()
+
+#////////////////////////////////////////////////////////////////////////
+
+#////////////////////////////////////////////////////////////////////////
+
 
 class Program(QWidget):
 
@@ -17,11 +47,12 @@ class Program(QWidget):
 #////////////////////////////////////////////////////////////////////////
     def Window(self):
 
-        msg = "TEST.Window"
+        msg = " Window "
 
         ukladT = QGridLayout()
 
         self.napis1Edt = QLineEdit()
+        self.napis1Edt.setFixedSize(300, 100)
         self.napis2Edt = QLineEdit()
 
         msg1 = QLabel(msg)
@@ -37,24 +68,29 @@ class Program(QWidget):
 
         self.napis1Edt.setReadOnly(True)
         ukladT.addWidget(self.napis1Edt, 1, 1, 2, 3)
-        ukladT.addWidget(BtnUp, 2, 2)
-        ukladT.addWidget(BtnRight, 2, 3)
-        ukladT.addWidget(BtnLeft, 2, 1)
-        ukladT.addWidget(self.napis2Edt, 3, 1, 2, 2)
-        ukladT.addWidget(BtnDo, 3, 3, 2, 3)
-        ukladT.addWidget(msg1, 0, 1)
+        ukladT.addWidget(BtnUp, 3, 2)
+        ukladT.addWidget(BtnRight, 3, 3)
+        ukladT.addWidget(BtnLeft, 3, 1)
+        ukladT.addWidget(self.napis2Edt, 4, 1, 2, 2)
+        ukladT.addWidget(BtnDo, 4, 3, 2, 1)
+        ukladT.addWidget(msg1, 0, 1, 1, 3)
         self.setLayout(ukladT)
-        self.resize(400, 300)
+        ukladH = QHBoxLayout()
+        ukladT.addLayout(ukladH, 2, 0, 1, 4)
+        self.setFixedSize(400, 300)
         self.move(300, 300)
-        self.setWindowTitle('Okno')
+        self.setWindowTitle('Adventure')
         self.show()
 #////////////////////////////////////////////////////////////////////////
-
+    
 #////////////////////////////////////////////////////////////////////////
+
+
     def up(self):
 
-        self.napis1Edt.setText("Up")
+        self.napis1Edt.setText(room.zone1())
 
+        self.napis1Edt.setText(room.zone2())
 #////////////////////////////////////////////////////////////////////////
 
 #////////////////////////////////////////////////////////////////////////
@@ -78,6 +114,5 @@ class Program(QWidget):
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    okno = Program()
+    okno = Main()
     sys.exit(app.exec_())
-#exit()
---
+++ Rooms.py
@@ -0,0 +1,10 @@
+class room():
+    def zone0():
+        room_0 = "???"
+        return room_0
+    def zone1():
+        room_1 = "Zone_1"
+        return room_1
+    def zone2():
+        room_2 = "Zone_2"
+        return room_2
